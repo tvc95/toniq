be3cf67d7b1d0bab910b219201f77d04d4fe2f90
tvc95 <9078587+tvc95@users.noreply.github.com>
2026-03-08 19:58:42 -0300

    Creating component pages and implementing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,17 @@
-import {
-  playChord,
-  playInterval,
-  stopAll,
-  playProgression,
-} from "./audio/AudioEngine";
 import "./App.css";
-import { useExercise } from "./hooks/useExercise";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import HistoryPage from "./pages/HistoryPage/HistoryPage";
+import Exercise from "./pages/Exercise/Exercise";
+import Results from "./pages/Results/Results";
 
-function App() {
-  const exercise = useExercise({
-    mode: "intervals",
-    difficulty: "beginner",
-    totalQuestions: 5,
-  });
-
+export default function App() {
   return (
-    <>
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="card w-80 text-center space-y-4">
-          <h1
-            className="text-3xl font-bold"
-            style={{ color: "var(--color-primary)" }}
-          >
-            Toniq
-          </h1>
-          <p style={{ color: "var(--color-text-secondary)" }}>
-            Ear Training App
-          </p>
-          <button className="btn-primary w-full">Começar</button>
-          <button className="btn-ghost w-full">Ver histórico</button>
-        </div>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/exercise" element={<Exercise />} />
+      <Route path="/results" element={<Results />} />
+      <Route path="/history" element={<HistoryPage />} />
+    </Routes>
   );
 }
-
-export default App;
