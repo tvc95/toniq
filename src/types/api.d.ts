5d5701b125342d31583e6335615c8f4454a12135
tvc95 <9078587+tvc95@users.noreply.github.com>
2026-03-12 20:28:58 -0300

    Add XP management functions to API for experience tracking

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,7 +1,20 @@
+interface XPData {
+  total_xp: number
+  level: number
+}
+
+interface AddXPResult {
+  total_xp: number
+  level: number
+  leveled_up: boolean
+}
+
 interface Window {
   api: {
     saveSession: (data: SessionData) => Promise<void>
     getHistory: () => Promise<SessionData[]>
     playAudio: (config: AudioConfig) => Promise<void>
+    getXP: () => Promise<XPData>
+    addXP: (amount: number) => Promise<AddXPResult>
   }
 }
